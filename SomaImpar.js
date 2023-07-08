160d83cbed0992c27dfdb3b55709c46df9e4c333
javier1 <jjg143.2011@gmail.com>
2023-07-08 13:16:37 -0300

    ver 3 criado por Javier Gonzalez: acrescentada la salida "lista dos numeros pares"

--- SomaImpar.js
+++ SomaImpar.js
@@ -2,15 +2,21 @@
 
 let lista = [5, 9, 10, 2, 6, 3];
 let lista1 = [];
+let lista2 = [];
 soma = 0;
 i = 0;
+j = 0;
 
 for (let item of lista) {
     if (item % 2 != 0) {
         soma += item;
         lista1[i] = item;
         i++;
+    } else {
+        lista2[j] = item;
+        j++;
     }
 }
 
 console.log("a soma dos impares da lista", lista1, "é", soma);
+console.log("a lista dos numeros pares é", lista2);
