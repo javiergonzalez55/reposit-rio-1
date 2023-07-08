6ba2e3e883ab666347265ad07df3eb6929a0fb7b
javier1 <jjg143.2011@gmail.com>
2023-07-08 13:05:00 -0300

    ver 2 criada por Javier Gonzalez: acertada a salida com a lista dos impares

--- SomaImpar.js
+++ SomaImpar.js
@@ -1,12 +1,16 @@
 //codigo que soma los numeros impares duma lista
 
 let lista = [5, 9, 10, 2, 6, 3];
+let lista1 = [];
 soma = 0;
+i = 0;
 
 for (let item of lista) {
     if (item % 2 != 0) {
         soma += item;
+        lista1[i] = item;
+        i++;
     }
 }
 
-console.log("a soma dos impares da lista", lista, "é", soma);
+console.log("a soma dos impares da lista", lista1, "é", soma);
